9adee94e080d237040d6ff8c373f72474ce3b0b5
root <alina.delossantos@gmail.com>
2022-06-01 14:05:17 -0700

    100

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""counts words on all hot posts on a subreddit"""
+import requests
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+     """prints nbr of words in hot posts on a specific subreddit
+
+     Args:
+        subreddit (str): subreddit
+        word_list (list): word list
+        instances (obj): words/count
+        after (str): next page
+        count (int): results with a macth
+    """
+
+     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+     headers = {
+         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+     }
+     params = {
+         "after": after,
+         "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
+     
