c013fb020c1fe77e6cc6e4e1d2038d98356084a3
root <alina.delossantos@gmail.com>
2022-06-09 18:36:18 -0700

    ex

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# set a higher limit
+  exec { 'file limit':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider => shell,
+}
